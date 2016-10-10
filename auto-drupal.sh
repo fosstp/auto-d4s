b3894ab274c3fdebf08b0982f5e42189b8277bc3
Alex Wang <yen867@gmail.com>
2016-10-10 21:30:03 +0800

    修正shell相容性, 美化 md files

--- auto-drupal.sh
+++ auto-drupal.sh
@@ -1,21 +1,22 @@
-printf "\E[0;32;40m"
-echo -e "臺北市自由軟體推動小組fosstp--自動化Drupal校園網站自動安裝程序\n"
-printf "\E[0m"
+#!/bin/bash
+printf "\e[0;32;40m"
+echo -e "臺北市自由軟體推動小組 fosstp -- 自動化 Drupal 校園網站自動安裝程序\n"
+printf "\e[0m"
 echo " ========================================================"
 
-printf "\E[0;35;40m"
+printf "\e[0;35;40m"
 echo -e "請先進行參數設定\n"
-printf "\E[0m"
+printf "\e[0m"
 echo " ========================================================"
-printf "\E[0;33;40m"
-read -p "請輸入您要使用的mysql root密碼(Please enter your mysql root password): " mysql_pw
+printf "\e[0;33;40m"
+read -p "請輸入您要使用的 mysql root 密碼(Please enter your mysql root password): " mysql_pw
 
-read -p "您是否要安裝phpmyadmin(Do you want to install phpmyadmin) Y/N: " pma_yn
+read -p "您是否要安裝 phpmyadmin (Do you want to install phpmyadmin) Y/N: " pma_yn
 
-read -p "請輸入您要使用的drupal站台名稱(Please enter your drupal site-name): "  drupal_sitename
+read -p "請輸入您要使用的 drupal 站台名稱(Please enter your drupal site-name): "  drupal_sitename
 
-read -p "請輸入您要使用的drupal管理員密碼(Please enter your drupal admin password): "  drupal_admin_pw
-printf "\E[0m"
+read -p "請輸入您要使用的 drupal 管理員密碼(Please enter your drupal admin password): "  drupal_admin_pw
+printf "\e[0m"
 echo " ========================================================"
 echo -e "偵測系統版本....."
 
@@ -31,33 +32,33 @@ elif [ -f /etc/redhat-release ]; then
 else
     DISTRO=$(uname -s)
 fi
-printf "\E[0;35;40m"
-echo -e "系統版本為:" $DISTRO " 開始進行docker安裝"
-printf "\E[0m"
+printf "\e[0;35;40m"
+echo -e "系統版本為: " $DISTRO " 開始進行 docker 安裝"
+printf "\e[0m"
 echo " ========================================================"
 
 case ${DISTRO} in
   "RedHat")
-	echo -e "於RedHat/CentOS 系統底下 安裝docker\n\n"
+	echo -e "於 RedHat / CentOS 系統底下 安裝 docker \n\n"
         yum -y install net-tools && curl -fsSL https://get.docker.com/ | sh
 	systemctl start docker
 	systemctl enable docker
 	chkconfig docker on
         ;;
   "Debian")
-        echo -e "於Debian/Ubuntu系統底下 安裝docker\n\n"
+        echo -e "於 Debian / Ubuntu 系統底下 安裝 docker \n\n"
 	apt-get update && apt-get --assume-yes install curl  && curl -fsSL https://get.docker.com/ | sh
         ;;
   "Ubuntu")
-        echo -e "於Debian/Ubuntu系統底下 安裝docker\n\n"
+        echo -e "於 Debian / Ubuntu 系統底下 安裝 docker \n\n"
 	apt-get update && apt-get --assume-yes install curl  && curl -fsSL https://get.docker.com/ | sh
         ;;
 esac
 
 
-printf "\E[0;35;40m"
-echo  -n "開始進行mysql docker部署動作...."
-printf "\E[0m"
+printf "\e[0;35;40m"
+echo  -n "開始進行 mysql docker 部署動作...."
+printf "\e[0m"
 echo " ========================================================"
 
 docker run --restart=always  --name mysql -e MYSQL_ROOT_PASSWORD=$mysql_pw -e MYSQL_DATABASE=drupal  -d mysql/mysql-server
@@ -85,9 +86,9 @@ printf "Complete\n"
 
 
 if [ "${pma_yn}" == "Y" ] || [ "${pma_yn}" == "y" ]; then
-	printf "\E[0;35;40m"
-	echo -n "開始進行phpmyadmin docker部署動作...."
-	printf "\E[0m"
+	printf "\e[0;35;40m"
+	echo -n "開始進行 phpmyadmin docker 部署動作...."
+	printf "\e[0m"
 	docker run --restart=always --name phpmyadmin --link mysql:db -p 8080:80 -d phpmyadmin/phpmyadmin
 	echo " ========================================================完成"
 	pmaurl= $(hostname -I | awk '{ print "http://"$1":8080/"}')
@@ -96,23 +97,23 @@ fi
 
 
 
-printf "\E[0;35;40m"
-echo -n "開始進行drupal docker部署動作...."
-printf "\E[0m"
+printf "\e[0;35;40m"
+echo -n "開始進行 drupal docker 部署動作...."
+printf "\e[0m"
 docker run --restart=always  --name drupal --link mysql:db -p 80:80 -p 443:443 -d fosstp/drupal
 echo " ========================================================完成"
 
 
 
-printf "\E[0;35;40m"
+printf "\e[0;35;40m"
 echo -n "開始進行 drupal 站台自動化安裝作業...."
-printf "\E[0m"
+printf "\e[0m"
 docker exec  drupal drush -y  site-install standard --clean-url=0 --site-name=$drupal_sitename --account-pass=$drupal_admin_pw --db-url=mysql://root:${mysql_pw}@db/drupal
 echo " ========================================================完成"
 
-printf "\E[0;35;40m"
+printf "\e[0;35;40m"
 echo -n "開始進行 drupal 中文化介面與校務模組安裝..."
-printf "\E[0m"
+printf "\e[0m"
 echo " ========================================================"
 
 docker exec  drupal cp /etc/php5/cli/php.ini  /usr/local/etc/php/
@@ -131,13 +132,13 @@ docker exec  drupal chown -R www-data:www-data /var/www/html/sites/default
 docker exec  drupal chmod -R 755 /var/www/html/sites/default/files
 docker restart drupal
 echo " ========================================================"
-printf "\E[0;32;40m"
-echo -e "安裝結束 您可以使用下列網址測試drupal是否安裝成功 系統管理員帳號為 admin 密碼為"  $drupal_admin_pw "\n"
+printf "\e[0;32;40m"
+echo -e "安裝結束 您可以使用下列網址測試 drupal 是否安裝成功 系統管理員帳號為 admin 密碼為"  $drupal_admin_pw "\n"
 hostname -I | awk '{ print "http://"$1"/"}'
 
 if [ "${pma_yn}" == "Y" ] || [ "${pma_yn}" == "y" ]; then
-        echo -e "\n您可以使用下列網址測試phpmyadmin是否安裝成功 phpmyadmin帳號為 root 密碼為"  $mysql_pw  "\n"
+        echo -e "\n您可以使用下列網址測試 phpmyadmin 是否安裝成功 phpmyadmin帳號為 root 密碼為"  $mysql_pw  "\n"
         hostname -I | awk '{ print "http://"$1":8080/"}'
 fi
 
-printf "\E[0m"
+printf "\e[0m"
--- d4s-ova.md
+++ d4s-ova.md
@@ -1,6 +1,4 @@
-
-
-Drupal4School Ova 虛擬機樣板使用說明：
+# Drupal4School Ova 虛擬機樣板使用說明：
 
 版本：20160926
 
@@ -8,14 +6,13 @@ Drupal4School Ova 虛擬機樣板使用說明：
 
 檔名與大小：D4S20160926.ova (1955634688 bytes)
 
-檔案驗證碼
+## 檔案驗證碼
 
 md5：95bd7b5566c83d8a9c2e9ab85b5c1d60
 
 sha1：b82f5d51c371adff3e3a048ae801374fd52ae4eb
 
-
-Ova樣板模擬硬體
+## Ova樣板模擬硬體
 
 CPU：2 core
 
@@ -25,13 +22,11 @@ HDD：320GB SATA
 
 Network：Virt-IO net (預設使用dhcp)
 
-
-Ova樣板使用軟體
+## Ova樣板使用軟體
 
 OS版本：Debian8.5 最小安裝含ssh server
 
 Docker版本：version 1.12.1, build 23cf638
-
 
 Drupal4School image版本：
 
@@ -45,59 +40,28 @@ PhpMyadmin image版本：
 
 2016-09-23T18:54:58.798084218Z sha256:fe8c9a43e85ba407ede642f3fb4f44984671d9db9c6583e1c86e525f97b85962
 
-
-Ova樣板相關帳號密碼
+## Ova樣板相關帳號密碼
 
 OS 管理員：root/12345678
 
 OS SSH遠端登入帳號(提供su權限)：d4s/12345678
 
 Drupal系統管理員:admin/12345678
+
 PhpMyadmin 管理員:root/12345678
 
 Mysql 管理員:root/12345678
 
 
 
-
-匯入操作說明：
+## 匯入操作說明：
 
 前置作業：建議先開啟主機板上VT-x相關虛擬化功能，避免OVA樣板運作異常
 
-
-1.請先安裝最新版VirtualBox軟體，下載網址：https://www.virtualbox.org/wiki/Downloads
-
-2.安裝完畢後開啟VirtualBox
-
-
-3.檔案->匯入應用裝置->匯入Ova檔案
-
-
-
-
-
-
-4.啟動虛擬機，修改Host網路卡
-
-
-
-
-
-5.啟動成功後請用root帳號登入，畫面顯示目前網站IP
-
-
-
-
-
-
-
-
-6.如要修改IP位址，請使用root權限輸入/root/ipconfig.sh，依照提示填寫IP設定資訊
-
-
-
-
-
-
-
+1. 請先安裝最新版VirtualBox軟體，下載網址：https://www.virtualbox.org/wiki/Downloads
+2. 安裝完畢後開啟VirtualBox
+3. 檔案->匯入應用裝置->匯入Ova檔案
+4. 啟動虛擬機，修改Host網路卡
+5. 啟動成功後請用root帳號登入，畫面顯示目前網站IP
+6. 如要修改IP位址，請使用root權限輸入/root/ipconfig.sh，依照提示填寫IP設定資訊
 
